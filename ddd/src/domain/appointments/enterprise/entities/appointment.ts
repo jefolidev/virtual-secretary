8340813fd9dbf7cdf062cc131974b1f61d079205
jefolidev <jefoliveira279@gmail.com>
2025-09-23 13:41:29 -0300

    update: added first entity of appointments + update: created first folders and files

---
+++ ddd/src/domain/appointments/enterprise/entities/appointment.ts
@@ -0,0 +1,111 @@
+import { Entity } from '../../../../core/entities/entity'
+import type { Optional } from '../../../../core/entities/types/optional'
+import { UniqueEntityId } from '../../../../core/entities/unique-entity-id'
+
+type AppointmentModalityType = 'IN_PERSON' | 'ONLINE'
+type AppointmentStatusType =
+  | 'SCHEDULED'
+  | 'CANCELED'
+  | 'CONFIRMED'
+  | 'NO_SHOW'
+  | 'COMPLETED'
+
+interface AppointmentProps {
+  clientId: UniqueEntityId
+  professionalId: UniqueEntityId
+  paymentId?: UniqueEntityId
+  startDateTime: Date
+  endDateTime: Date
+  modality: AppointmentModalityType
+  status: AppointmentStatusType
+  extraPreferences?: string
+  googleMeetLink?: string
+  createdAt: Date
+  updatedAt?: Date
+}
+
+export class Appointment extends Entity<AppointmentProps> {
+  get clientId() {
+    return this.props.clientId
+  }
+
+  get professionalId() {
+    return this.props.professionalId
+  }
+
+  get paymentId() {
+    return this.props.paymentId
+  }
+
+  get startDateTime() {
+    return this.props.startDateTime
+  }
+
+  set startDateTime(startDateTime: Date) {
+    this.props.startDateTime = startDateTime
+  }
+
+  get endDateTime() {
+    return this.props.endDateTime
+  }
+
+  set endDateTime(endDateTime: Date) {
+    this.props.endDateTime = endDateTime
+  }
+
+  get modality() {
+    return this.props.modality
+  }
+
+  set modality(modality: AppointmentModalityType) {
+    this.props.modality = modality
+  }
+
+  get status() {
+    return this.props.status
+  }
+
+  set status(status: AppointmentStatusType) {
+    this.props.status = status
+  }
+
+  get extraPreferences() {
+    return this.props.extraPreferences ?? 'No extra preferences'
+  }
+
+  set extraPreferences(extraPreferences: string) {
+    this.props.extraPreferences = extraPreferences
+  }
+
+  get googleMeetLink() {
+    return this.props.googleMeetLink ?? 'No google meet link'
+  }
+
+  set googleMeetLink(googleMeetLink: string) {
+    this.props.googleMeetLink = googleMeetLink
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt ?? new Date()
+  }
+
+  set updatedAt(updatedAt: Date) {
+    this.props.updatedAt = updatedAt
+  }
+
+  static create(
+    props: Optional<AppointmentProps, 'createdAt'>,
+    id?: UniqueEntityId
+  ) {
+    const appointment = new Appointment(
+      { ...props, createdAt: props.createdAt ?? new Date() },
+      id
+    )
+
+    return appointment
+  }
+}
