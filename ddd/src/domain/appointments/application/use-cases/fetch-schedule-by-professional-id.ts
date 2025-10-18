1fd7c3cbfde9778715cd84ce99f445339ed0fb0d
jefolidev <jefoliveira279@gmail.com>
2025-10-18 05:31:50 -0300

    update: added appointment fetch by professional id

---
+++ ddd/src/domain/appointments/application/use-cases/fetch-schedule-by-professional-id.ts
@@ -0,0 +1,29 @@
+import { type Either, right } from '@src/core/either'
+import { UniqueEntityId } from '@src/core/entities/unique-entity-id'
+import type { Appointment } from '../../enterprise/entities/appointment'
+import type { AppointmentsRepository } from '../repositories/appointments.repository'
+import type { NotFoundError } from './errors/resource-not-found-error'
+
+export interface FetchScheduleByProfessionalIdUseCaseProps {
+  professionalId: string
+}
+
+type FetchScheduleByProfessionalIdUseCaseResponse = Either<
+  NotFoundError,
+  { appointments: Appointment[] | [] }
+>
+
+export class FetchScheduleByProfessionalIdUseCase {
+  constructor(private appointmentsRepository: AppointmentsRepository) {}
+
+  async execute({
+    professionalId,
+  }: FetchScheduleByProfessionalIdUseCaseProps): Promise<FetchScheduleByProfessionalIdUseCaseResponse> {
+    const appointments =
+      await this.appointmentsRepository.findManyByProfessionalId(
+        new UniqueEntityId(professionalId)
+      )
+
+    return right({ appointments })
+  }
+}
