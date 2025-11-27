6a1e418acb3790a098848a462a6ed16f7bd5ca71
jefolidev <jefoliveira279@gmail.com>
2025-11-26 21:24:52 -0300

    update: added sesion jwt auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from './auth/auth.module'
+import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { envSchema } from './env'
 import { PrismaService } from './prisma/prisma.service'
@@ -11,8 +13,9 @@ import { PrismaService } from './prisma/prisma.service'
       envFilePath: '.env',
       validate: (env) => envSchema.parse(env),
     }),
+    AuthModule,
   ],
-  controllers: [CreateAccountController],
+  controllers: [AuthenticateController, CreateAccountController],
   providers: [PrismaService],
 })
 export class AppModule {}
--- backend/src/env.ts
+++ backend/src/env.ts
@@ -4,6 +4,8 @@ export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   PORT: z.coerce.number().optional().default(3333),
   NODE_ENV: z.string().default('development'),
+  JWT_PUBLIC_KEY: z.string(),
+  JWT_PRIVATE_KEY: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>
