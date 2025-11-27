d52dba788727d5f635e5fbcc858a160909c5ac6e
jefolidev <jefoliveira279@gmail.com>
2025-11-27 14:25:10 -0300

    update: added user decorator to get the user id by header

--- backend/src/controllers/create-organization.controller.ts
+++ backend/src/controllers/create-organization.controller.ts
@@ -1,16 +1,17 @@
+import { CurrentUser } from '@/auth/current-user-decorator'
+import { JwtAuthGuard, UserPayload } from '@/auth/jwt.strategy'
 import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
 import { PrismaService } from '@/prisma/prisma.service'
 import { Controller, Post, UseGuards, UsePipes } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
-import { AuthGuard } from '@nestjs/passport'
 import z from 'zod'
 
 const createOrganizationSchema = z.object({
-  name: z.string(),
+  name: z.string().optional(),
 })
 
 @Controller('/organization')
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(JwtAuthGuard)
 export class CreateOrganizationController {
   constructor(
     private prisma: PrismaService,
@@ -19,7 +20,7 @@ export class CreateOrganizationController {
 
   @Post()
   @UsePipes(new ZodValidationPipe(createOrganizationSchema))
-  async handle() {
-    return 'oi'
+  async handle(@CurrentUser() user: UserPayload) {
+    return user.sub
   }
 }
