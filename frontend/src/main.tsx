c54c5849507fdbc335fcf7526ec28327c3b111f8
jefolidev <jefoliveira279@gmail.com>
2026-01-05 12:01:39 -0300

    chore: update dependencies and improve main app configuration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router/dom'
+import { AuthProvider } from './contexts/auth-context'
 import './index.css'
 import { router } from './routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className="w-full h-screen ">
-      <RouterProvider router={router} />
-    </div>
+    <AuthProvider>
+      <div className="w-full h-screen ">
+        <RouterProvider router={router} />
+      </div>
+    </AuthProvider>
   </StrictMode>
 )
