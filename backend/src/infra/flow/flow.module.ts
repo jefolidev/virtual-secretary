4ba3ac4fe6422755237d0696e909d41b771a097a
jefolidev <jefoliveira279@gmail.com>
2026-01-30 17:16:58 -0300

    feat(flow): cria módulo FlowModule para centralizar serviços de fluxo conversacional

---
+++ backend/src/infra/flow/flow.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common'
+import { DatabaseModule } from '../database/database.module'
+import { OpenAIModule } from '../webhooks/openai/openai.module'
+import { FlowService } from './flow.service'
+import { AppointmentFlowService } from './flows/appointment-flow.service'
+import { RegistrationFlowService } from './flows/registration-flow.service'
+
+@Module({
+  exports: [FlowModule, AppointmentFlowService, RegistrationFlowService],
+  imports: [DatabaseModule, OpenAIModule],
+  providers: [FlowService, AppointmentFlowService, RegistrationFlowService],
+})
+export class FlowModule {}
