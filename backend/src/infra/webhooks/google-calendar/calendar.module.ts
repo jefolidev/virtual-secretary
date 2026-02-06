1e6ac95682d24b79abadc913a26eb2a5f59b732b
jefolidev <jefoliveira279@gmail.com>
2026-02-06 15:57:56 -0300

    feat: register google auth controller and authenticate use case in calendar module

--- backend/src/infra/webhooks/google-calendar/calendar.module.ts
+++ backend/src/infra/webhooks/google-calendar/calendar.module.ts
@@ -1,11 +1,16 @@
+import { OnAppointmentCreated } from '@/domain/scheduling/application/subscribers/on-appointment-created'
+import { AuthenticateWithGoogleUseCase } from '@/domain/scheduling/application/use-cases/authenticate-user-with-google'
+import { CheckGoogleCalendarConnectionUseCase } from '@/domain/scheduling/application/use-cases/check-google-calendar-connection'
 import { CreateCalendarEventUseCase } from '@/domain/scheduling/application/use-cases/create-calendar-event'
 import { FetchProfessionalEventsUseCase } from '@/domain/scheduling/application/use-cases/fetch-professional-events'
 import { GetAuthUrlUseCase } from '@/domain/scheduling/application/use-cases/get-google-auth-url'
 import { HandleOAuthCallbackUseCase } from '@/domain/scheduling/application/use-cases/handle-oauth-callback'
 import { DatabaseModule } from '@/infra/database/database.module'
 import { Module } from '@nestjs/common'
+import { CheckGoogleConnectionController } from './controllers/check-google-connection.controller'
 import { GetAuthUrlController } from './controllers/get-auth-url.controller'
 import { GetEventsController } from './controllers/get-events.controller'
+import { GoogleAuthController } from './controllers/google-auth.controller'
 import { HandleOAuthCallbackController } from './controllers/handle-oauth-callback.controller'
 
 @Module({
@@ -14,12 +19,17 @@ import { HandleOAuthCallbackController } from './controllers/handle-oauth-callba
     GetAuthUrlController,
     GetEventsController,
     HandleOAuthCallbackController,
+    CheckGoogleConnectionController,
+    GoogleAuthController,
   ],
   providers: [
     CreateCalendarEventUseCase,
     FetchProfessionalEventsUseCase,
     GetAuthUrlUseCase,
     HandleOAuthCallbackUseCase,
+    CheckGoogleCalendarConnectionUseCase,
+    AuthenticateWithGoogleUseCase,
+    OnAppointmentCreated,
   ],
   exports: [],
 })
