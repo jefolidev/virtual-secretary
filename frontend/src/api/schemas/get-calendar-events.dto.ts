1c6c678d10b0cc08130732b9056feca332a9b766
jefolidev <jefoliveira279@gmail.com>
2026-02-13 13:57:59 -0300

    feat(frontend): frontend updates - frontend/src/api/schemas/get-calendar-events.dto.ts

--- frontend/src/api/schemas/get-calendar-events.dto.ts
+++ frontend/src/api/schemas/get-calendar-events.dto.ts
@@ -1,2 +1,14 @@
-
-
+export interface GetCalendarEventsResponse {
+  id: string
+  appointmentId: string
+  professionalId: string
+  googleEventLink: string
+  summary: string
+  description: string
+  startDateTime: Date
+  endDateTime: Date
+  syncStatus: string
+  lastSyncedAt: Date
+  createdAt: Date
+  updatedAt: Date
+}
