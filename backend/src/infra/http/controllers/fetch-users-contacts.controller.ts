27b7d993396dfd796d3d9cf34db31f0fe4dda80a
jefolidev <jefoliveira279@gmail.com>
2026-02-13 13:58:01 -0300

    feat(controller): controller changes - backend/src/infra/http/controllers/fetch-users-contacts.controller.ts

---
+++ backend/src/infra/http/controllers/fetch-users-contacts.controller.ts
@@ -0,0 +1,25 @@
+import { UserRepository } from '@/domain/scheduling/application/repositories/user.repository'
+import { Controller, Get } from '@nestjs/common'
+import { UserClientWhatsappPresenter } from '../presenters/user-client-whatsapp-presenter'
+import { WhatsappContactPresenter } from '../presenters/whatsapp-contact-presenter'
+
+@Controller('contacts')
+export class FetchUsersContactsController {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  @Get('users')
+  async fetchAll() {
+    const users = await this.userRepository.fetchWhatsappRegisteredAndUnlinked()
+
+    if (!users) return []
+
+    const presentedRegistredUsers = users.registred.map((registredUser) =>
+      UserClientWhatsappPresenter.toHTTP(registredUser),
+    )
+    const presentedUnlinkedUsers = users.unlinked.map((unlinkedUser) =>
+      WhatsappContactPresenter.toHTTP(unlinkedUser),
+    )
+
+    return [...presentedRegistredUsers, ...presentedUnlinkedUsers]
+  }
+}
