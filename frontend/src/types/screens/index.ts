56e9f0e9577c1f61d4ed845453a9cf6f2588a958
jefolidev <jefoliveira279@gmail.com>
2026-02-13 13:58:00 -0300

    feat(frontend): frontend updates - frontend/src/types/screens/index.ts

--- frontend/src/types/screens/index.ts
+++ frontend/src/types/screens/index.ts
@@ -1,8 +1,11 @@
 export const ScreensEnum = {
   LOGIN: 'login',
   SIGNUP: 'signup',
-  DASHBOARD: 'dashboard',
+  CALENDAR: 'calendar',
   SETTINGS: 'settings',
+  CLIENTS: 'clients',
+  FINANCE: 'finance',
+  ANALYTICS: 'analytics',
 } as const
 
 export type ScreensEnum = (typeof ScreensEnum)[keyof typeof ScreensEnum]
