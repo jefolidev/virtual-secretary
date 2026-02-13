e915600eab73a2b49e37916e95d3f01f6dde9749
jefolidev <jefoliveira279@gmail.com>
2026-02-13 13:57:58 -0300

    feat(controller): controller changes - backend/src/infra/http/controllers/dto/forgot-password.dto.ts

--- backend/src/infra/http/controllers/dto/forgot-password.dto.ts
+++ backend/src/infra/http/controllers/dto/forgot-password.dto.ts
@@ -1,6 +1,7 @@
 import z from 'zod'
 
 export const forgotPasswordBodySchema = z.object({
+  email: z.email('Invalid email address'),
   newPassword: z.string().min(6, 'Password must be at least 6 characters'),
   newPasswordConfirmation: z
     .string()
